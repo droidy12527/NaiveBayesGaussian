c4f1b64a995c2516813b8326556c4440c8c62070
droidy12527 <gundiaramasu@gmail.com>
2020-03-11 12:45:33 +0530

    V2.0

--- gaussianNN.py
+++ gaussianNN.py
@@ -1,6 +1,8 @@
 from sklearn import preprocessing
 from sklearn.naive_bayes import GaussianNB
-
+weather_main = ['overcast', 'rainy', 'sunny']
+play_main = ['No','Yes']
+temp_main = ['cool','hot','mild']
 weather=['Sunny','Sunny','Overcast','Rainy','Rainy','Rainy','Overcast','Sunny','Sunny','Rainy','Sunny','Overcast','Overcast','Rainy']
 temp=['Hot','Hot','Hot','Mild','Cool','Cool','Cool','Mild','Cool','Mild','Mild','Mild','Hot','Mild']
 play=['No','No','Yes','Yes','Yes','No','Yes','No','Yes','Yes','Yes','Yes','Yes','No']
@@ -11,4 +13,9 @@ play_en = label.fit_transform(play)
 features =list(zip(weather_en, temp_en))
 gaussian = GaussianNB()
 gaussian.fit(features, play_en)
-print(gaussian.predict([[0,2]]))
+weather_ip = input('Tell me weather condition: Sunny / Overcast / Rainy: ')
+weather_ip = weather_main.index(weather_ip.lower())
+temp_ip = input('Tell me temperature: Hot / Mild / Cool: ')
+temp_ip = temp_main.index(temp_ip.lower())
+prediction = int(gaussian.predict([[weather_ip,temp_ip]]))
+print('The prediction is: '+play_main[prediction])
